6cc2995e78a5616293fbc3021cc7ad80fea54616
John Ward <john@johnward.net>
2020-07-15 22:42:57 +0100

    Created a window

--- main.c
+++ main.c
@@ -1,12 +1,40 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <wayland-server.h>
+#include <wlr/backend.h>
+#include <wlr/render/wlr_renderer.h>
 
-struct mcw_server
+struct ward_server
 {
     struct wl_display *wl_display;
     struct wl_event_loop *wl_event_loop;
+
+    struct wlr_backend *backend;
 };
 
 int main(int argc, char **argv)
 {
+    struct ward_server server;
+    server.wl_display = wl_display_create();
+    assert(server.wl_display);
+    server.wl_event_loop = wl_display_get_event_loop(server.wl_display);
+    assert(server.wl_event_loop);
+
+    server.backend = wlr_backend_autocreate(server.wl_display, NULL);
+    assert(server.backend);
+
+    if (!wlr_backend_start(server.backend))
+    {
+        fprintf(stderr, "Failed to start backend\n");
+        wl_display_destroy(server.wl_display);
+        return 1;
+    }
+
+    wl_display_run(server.wl_display);
+    wl_display_destroy(server.wl_display);
+
     printf("Hello Wardland\n");
+    return 0;
 }
